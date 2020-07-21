628b4732513074c5a7297fd32b39f96108e81030
Patrick Gaines <patrickg.codes@gmail.com>
2020-07-21 10:46:29 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, NavLink, Switch } from "react-router-dom";
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class component
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="App">
+        <div className="nav">
+          <NavLink exact to="/" activeClassName="active">Home</NavLink>
+          <NavLink to="/about" activeClassName="active">About</NavLink>
+        </div>
+        <Route render={({location}) =>(
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={950} classNames="fade">
+              <Switch location={location}>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )} />
+
+      </div>
+    );
+  }
 }
 
 export default App;
